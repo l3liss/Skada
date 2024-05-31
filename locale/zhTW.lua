07d98497437a20910c4aed1154489b300cd9b256
l3liss <s1986f@gmail.com>
2024-05-31 18:30:21 -0400

    Delete locale/zhTW.lua

--- locale/zhTW.lua
+++
@@ -1,4 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("Skada", "zhTW", false)
-if not L then return end
-
---@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
